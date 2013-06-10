b630f25d4ce7e4b954942fc57cd353863d30e45b
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-06-10 19:47:39 +0100

    Fixes #24

--- leakcheck.js
+++ leakcheck.js
@@ -1,12 +1,15 @@
 var SP = require('./build/Release/node_stringprep.node');
-
+var useSetImmediate = (parseFloat(process.version.replace('v', '')) >= 0.10)
 function run() {
     var p = new SP.StringPrep('nameprep');
     var r = p.prepare('A\u0308ffin');
     if (r !== 'äffin')
 	throw r;
 
-    process.nextTick(run);
+    if (true == useSetImmediate)
+        setImmediate(run)
+    else
+        process.nextTick(run)
 }
 
 try {
